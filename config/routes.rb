36f1b75b4cf209b5686d284b684ca67ff0b7c06e
kyu__yui <chiuyuf61139@gmail.com>
2020-12-05 19:14:16 +0800

    Merge branch 'dev/1' of https://github.com/5xruby-ecommerce/shopp into kyuline
    # Conflicts:
    #	config/routes.rb
    Modify conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,3 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  resources :products
-
-  resources :cart, only:[:show, :destroy]do
-    collection do
-      post :add, path:'add/:id'
-    end
-  end
-=======
   resources :users
->>>>>>> d8d4c43f3faf7a469c82daee224dc0ce61576b2f
 end
