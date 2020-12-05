43b6d3c1b6608009dce416ea16fc8f8093b51653
kyu__yui <chiuyuf61139@gmail.com>
2020-12-05 12:49:10 +0800

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+  resources :products
 
+  resources :cart, only:[:show, :destroy]do
+    collection do
+      post :add, path:'add/:id'
+    end
+  end
+end
